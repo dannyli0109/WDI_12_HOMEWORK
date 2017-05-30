48d5f103ad2ba505b1c1d338f123fecf4826533c
Danny Li <dannyli0109@gmail.com>
2017-05-30 15:30:32 +1000

    day2-homework

---
+++ Danny/wk01/2-tue/app.js
@@ -0,0 +1,53 @@
+// even/odd
+for (var num = 0; num <= 20; num ++) {
+  if (num % 2 == 0) {
+    console.log(num + " is even");
+  } else {
+    console.log(num + " is odd");
+  }
+}
+
+// multiplication tables
+for (var first = 1; first <= 10; first ++) {
+  var returnString = "";
+  for (var second = 1; second <= 10; second ++) {
+    returnString += "[" + (first * second) + "]"
+  }
+  console.log(returnString);
+}
+
+
+// you top choices
+var colors = ["blue", "red", "green", "yellow", "brown", "gray", "gold", "silver"];
+
+for (var i = 0; i < colors.length; i++) {
+  var returnString = "My " + (i + 1);
+  var sNum = (i + 1).toString();
+  var lastDigit = sNum.charAt(sNum.length - 1);
+
+  if (lastDigit == 1) {
+    returnString += "st "
+  } else if (lastDigit == 2) {
+    returnString += "nd "
+  } else if (lastDigit == 3) {
+    returnString += "rd "
+  } else {
+    returnString += "th "
+  }
+  returnString += "choice is " + colors[i];
+  console.log(returnString);
+}
+
+
+// years
+var year = 2018
+var currentDate = new Date();
+var currentYear = currentDate.getFullYear();
+
+if (year === currentYear) {
+  console.log("I'm in the present!");
+} else if (year > currentYear) {
+  console.log("Greetings from the future");
+} else {
+  console.log("Whoa! Blast from the past!");
+}
