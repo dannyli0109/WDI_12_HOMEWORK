99c884ab39bad531e3ab1f6fcc2410779727fb15
Danny Li <dannyli0109@gmail.com>
2017-06-28 22:14:16 +1000

    movie3

--- Danny/wk05/1-mon/movie/main.rb
+++ Danny/wk05/1-mon/movie/main.rb
@@ -3,15 +3,55 @@ require 'sinatra'
 require 'httparty'
 require 'pry'
 require 'sinatra/reloader'
+require 'pg'
 
 apikey = "2f6435d9"
+
+def run_sql(sql)
+  conn = PG.connect(dbname: 'movie_db')
+  result = conn.exec(sql)
+  conn.close
+  return result
+end
+
+def add_movie id
+  columns = ["imdbID", "Title", "Year", "Rated", "Released", "Genre", "Director", "Writer", "Actors", "Poster"]
+  apikey = "2f6435d9"
+
+  find_exist = "SELECT * FROM movies WHERE imdbID = '#{id}'"
+  if run_sql(find_exist).count == 0
+    @result = HTTParty.get("http://www.omdbapi.com/?i=#{id}&apikey=#{apikey}").parsed_response
+
+    i = 0
+    value_array = []
+
+    while i < columns.length
+      result_copy = @result[columns[i]]
+      result_copy = result_copy.gsub(/'/,"''")
+      value_array.push("'" + result_copy + "'")
+      i += 1
+    end
+    sql = "INSERT INTO movies (#{columns.join(", ")}) VALUES (#{value_array.join(", ")})"
+    run_sql(sql)
+  else
+    @result = run_sql(find_exist)[0]
+    @result = Hash[@result.map {|key, value| [key.capitalize, value] }]
+  end
+end
+
+
 
 get '/' do
   erb :index
 end
 
-get "/search_result" do
+get "/movies" do
   movie = params["movie"]
+  movie = movie.tr(
+  "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
+  "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
+)
+
   @result = HTTParty.get("http://www.omdbapi.com/?s=#{movie}&apikey=#{apikey}").parsed_response
   FileUtils.cd("public") do
     if !File.directory?("history")
@@ -29,20 +69,20 @@ get "/search_result" do
     end
   end
 
-
-
   if @result["Error"] != nil
     erb :error
   elsif @result["Search"].count == 1
-    @result = HTTParty.get("http://www.omdbapi.com/?i=#{@result["Search"][0]["imdbID"]}&apikey=#{apikey}").parsed_response
+    id = @result["Search"][0]["imdbID"]
+    add_movie id
     erb :movie_details
   else
     erb :search_result
   end
 end
 
-get "/movie_details/:id" do
-  title = params[:id]
-  @result = HTTParty.get("http://www.omdbapi.com/?i=#{title}&apikey=#{apikey}").parsed_response
+get "/movies/:id" do
+  id = params[:id]
+  # hash of the movie
+  add_movie id
   erb :movie_details
 end
---
+++ Danny/wk05/1-mon/movie/movie.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE movies(
+  imdbID VARCHAR(400) PRIMARY KEY,
+  Title VARCHAR(400),
+  Year VARCHAR(400),
+  Rated VARCHAR(10),
+  Released VARCHAR(400),
+  Genre VARCHAR(400),
+  Director VARCHAR(400),
+  Writer VARCHAR(400),
+  Actors VARCHAR(400),
+  Poster text
+);
